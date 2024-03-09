ebbecc3ecf77e803c832d24a0ee8ff468af2abf7
PerevoznikCristian <cperevoznik@gmail.com>
2024-03-10 00:07:55 +0200

    Add login controler

---
+++ EfitSolution/ConsoleApp1/ClientBusiness.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public class ClientBusiness : IClientBusines
+     {
+          public string GetClientName(int ClientId)
+          {
+               throw new NotImplementedException();
+          }
+     }
+}
---
+++ EfitSolution/Efit.Busines/Interfaces/IClientBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efit.Busines.Interfaces
+{
+     public interface IClientBusiness
+     {
+          string GetClientInfo(int ClientId);
+     }
+}
